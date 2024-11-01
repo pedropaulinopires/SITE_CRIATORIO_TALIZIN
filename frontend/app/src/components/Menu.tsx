86a41056c540ca3d132ccf8775b71a4cc0de4ac1
Pedro Henrique Paulino Pires <121137670+pedropaulinopires@users.noreply.github.com>
2024-11-01 18:54:38 -0300

    Ajustando menu

--- frontend/app/src/components/Menu.tsx
+++ frontend/app/src/components/Menu.tsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "../style/Menu.module.css"
 import logo_criatorio from "../svg/logo.svg"
 
+
 const Menu: React.FC = () => {
+
+    const [menuActive, setMenuActive] = useState(false);
+    const toggleMenu = () => { setMenuActive(x => !x) };
+    window.addEventListener('resize', () => setMenuActive(false));
+
     return (
-        <header className={style.menu}>
-            <img src={logo_criatorio} alt="logo-criatorio-talizin" />
+        <>
+            
+            <header className={style.menu}>
+                {/* Logo */}
+                <img src={logo_criatorio} alt="logo-criatorio-talizin" />
 
-            <div className={style.btn_menu}>
-                <div className={style.line1}></div>
-                <div className={style.line2}></div>
-                <div className={style.line3}></div>
+                {/* Botão menu mobile */}
+                <div className={`${style.btn_menu} ${menuActive ? style.active : ''}`} onClick={toggleMenu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </header>
+
+            {/* Conteudo menu mobile*/}
+            <div className={``}>
             </div>
-
-        </header>
+        </>
     )
 }
 
--- frontend/app/src/pages/Home.tsx
+++ frontend/app/src/pages/Home.tsx
@@ -5,6 +5,18 @@ const Home: React.FC = () => {
     return (
         <>
             <Menu />
+            <main>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consequuntur vitae debitis consectetur veritatis dolores necessitatibus atque consequatur vel voluptatem? Tenetur minus obcaecati et maiores magnam, unde sequi officia quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consequuntur vitae debitis consectetur veritatis dolores necessitatibus atque consequatur vel voluptatem? Tenetur minus obcaecati et maiores magnam, unde sequi officia quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consequuntur vitae debitis consectetur veritatis dolores necessitatibus atque consequatur vel voluptatem? Tenetur minus obcaecati et maiores magnam, unde sequi officia quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consequuntur vitae debitis consectetur veritatis dolores necessitatibus atque consequatur vel voluptatem? Tenetur minus obcaecati et maiores magnam, unde sequi officia quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consequuntur vitae debitis consectetur veritatis dolores necessitatibus atque consequatur vel voluptatem? Tenetur minus obcaecati et maiores magnam, unde sequi officia quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consequuntur vitae debitis consectetur veritatis dolores necessitatibus atque consequatur vel voluptatem? Tenetur minus obcaecati et maiores magnam, unde sequi officia quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consequuntur vitae debitis consectetur veritatis dolores necessitatibus atque consequatur vel voluptatem? Tenetur minus obcaecati et maiores magnam, unde sequi officia quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consequuntur vitae debitis consectetur veritatis dolores necessitatibus atque consequatur vel voluptatem? Tenetur minus obcaecati et maiores magnam, unde sequi officia quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consequuntur vitae debitis consectetur veritatis dolores necessitatibus atque consequatur vel voluptatem? Tenetur minus obcaecati et maiores magnam, unde sequi officia quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consequuntur vitae debitis consectetur veritatis dolores necessitatibus atque consequatur vel voluptatem? Tenetur minus obcaecati et maiores magnam, unde sequi officia quos.
+            </main>
         </>
     )
 }
